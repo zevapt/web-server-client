c1a38c057e4388d47d97fe3e5a1bb6d8b7693d10
Zeva Patu Assyadid <122035948+zevapt@users.noreply.github.com>
2023-05-04 23:29:09 +0700

    Create server.py

---
+++ web/server.py
@@ -0,0 +1,48 @@
+import socket
+import os
+
+# konfigurasi server
+server_address = ('localhost', 8080)
+
+# buat socket TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind socket ke alamat dan port tertentu
+server_socket.bind(server_address)
+
+# listen untuk koneksi masuk
+server_socket.listen(1)
+
+print(f"Sedang menunggu koneksi di {server_address}...")
+
+while True:
+    # terima koneksi dari client
+    client_socket, client_address = server_socket.accept()
+
+    print(f"Menerima koneksi dari {client_address}")
+
+    # terima data dari client
+    request_data = client_socket.recv(1024)
+
+    # parsing HTTP request
+    request_str = request_data.decode()
+    request_method, request_path, request_protocol = request_str.split('\n')[0].split()
+
+    print(f"HTTP request method: {request_method}")
+    print(f"HTTP request path: {request_path}")
+    print(f"HTTP request protocol: {request_protocol}")
+
+    # cari file yang diminta oleh client
+    file_path = "." + request_path
+    if os.path.isfile(file_path):
+        # buat HTTP response message dengan status code 200 OK
+        response_data = f"HTTP/1.1 200 OK\r\n\r\n{open(file_path).read()}".encode()
+    else:
+        # buat HTTP response message dengan status code 404 Not Found
+        response_data = b"HTTP/1.1 404 Not Found\r\n\r\nFile not found"
+
+    # kirim response message ke client
+    client_socket.sendall(response_data)
+
+    # tutup koneksi dengan client
+    client_socket.close()
